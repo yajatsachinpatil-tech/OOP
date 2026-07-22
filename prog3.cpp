fc16280083f56aee187c48ab0ac5bbd759fc0112
yajatsachinpatil-tech <yajatsachinpatil@gmail.com>
2026-07-22 14:47:57 +0530

    Create prog3.cpp

---
+++ prog3.cpp
@@ -0,0 +1,18 @@
+// code by yajat patil cse b 8
+#include <iostream>
+using namespace std;
+
+int main() {
+    float length, width;
+
+    cout << "Enter length: ";
+    cin >> length;
+
+    cout << "Enter width: ";
+    cin >> width;
+
+    cout << "Area = " << length * width << endl;
+    cout << "Perimeter = " << 2 * (length + width);
+
+    return 0;
+}
