306bdc0f2ad2a5b7d912ef8557ff3e7f930cb472
yajatsachinpatil-tech <yajatsachinpatil@gmail.com>
2026-07-22 14:47:12 +0530

    Create prog2.cpp

---
+++ prog2.cpp
@@ -0,0 +1,17 @@
+// code by yajat patil cse b 8
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Addition = " << a + b << endl;
+    cout << "Subtraction = " << a - b << endl;
+    cout << "Multiplication = " << a * b << endl;
+    cout << "Division = " << (float)a / b << endl;
+
+    return 0;
+}
